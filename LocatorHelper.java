ecb3f5db1d2eada0132730ab715241480d4c8839
wrmay <rmay@pivotal.io>
2018-07-06 15:56:05 -0400

    add LocatorHelper

---
+++ LocatorHelper.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Handles parsing of locator information
+ * @author rmay
+ *
+ */
+public class LocatorHelper {
+	private static final Logger _log = LogManager.getLogger(LocatorHelper.class);
+
+	private static Pattern LOCATOR_PATTERN= Pattern.compile("(\\S+)\\[(\\d{1,5})\\]");
+
+	private String host;
+	private int port;
+
+	public LocatorHelper(String host, int port){
+		this.host = host;
+		this.port = port;
+	}
+
+	public String getHost() {
+		return host;
+	}
+
+	public int getPort() {
+		return port;
+	}
+
+	public static List<LocatorHelper> parseLocatorList(String in){
+		String []locatorStrings = in.split(",");
+		ArrayList<LocatorHelper> result = new ArrayList<LocatorHelper>(locatorStrings.length);
+
+		for(String l: locatorStrings){
+			result.add(parseLocatorString(l));
+		}
+
+		return result;
+	}
+
+	public static LocatorHelper parseLocatorString(String in){
+		String host;
+		int port;
+
+		Matcher m = LOCATOR_PATTERN.matcher(in);
+		if (!m.matches()){
+			throw new LoggedException(String.format("'%s' does not have the correct format for a locator String.", in),_log);
+		} else{
+			host = m.group(1);
+			try {
+				port = Integer.parseInt(m.group(2));
+			} catch(NumberFormatException x){
+				throw new LoggedException(String.format("'%s' does not have the correct format for a locator String. The portion in square brackets could not be parsed as an integer.", in),_log);
+			}
+		}
+
+		return new LocatorHelper(host,port);
+	}
+
+
+}
